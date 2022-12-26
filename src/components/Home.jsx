8ed1eb959581640cccfac2bf257c4f7552463615
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2022-12-26 17:24:55 -0600

    fixed typo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Abdurraouf Etagiuir</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am a Software Developer</h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I am currently in my 3rd year Computer Scinece major, enrolled in 
-                University of Saskatchewan. I love developing and creating new Projects 
+                University of Saskatchewan. I love developing and creating new projects 
                 in either a group setting or alone. */</p>
             <div>
                 <a href="https://drive.google.com/file/d/1aTu-VKaT4mujMH9Bklxffax00yAMrgoN/view?usp=sharing">
