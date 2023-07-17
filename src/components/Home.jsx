f5e3d0cdc59e3a59aa73f3f62c90955b29b2739d
Abdurraouf Etagiuri <roofa.etagiuri1@gmail.com>
2023-07-16 19:43:01 -0600

    fixed a spelling mistake

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         {/* Container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className='text-2xl text-blue-500'>/* Hello! My name is</p>
-            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Abdurraouf Etagiuir</h1>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Abdurraouf Etagiuri</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am a Software Developer</h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I am currently in my 3rd year Computer Scinece major, enrolled in 
                 University of Saskatchewan. I love developing and creating new projects 
