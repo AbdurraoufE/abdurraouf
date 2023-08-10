6dccc74be02336b479da273b44eadb7eff9a6f74
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2023-08-10 00:41:39 -0600

    added changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ import comingSoon from '../assets/Soon.png'
 
 const Projects = () => {
   return (
-    <div name='projects' className='w-full md:h-screen text-gray-300 bg-[#0D1116]'>
+    <div name='projects' className='space-y-10 w-full md:h-screen text-gray-300 bg-[#0D1116]'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-blue-600'>Projects</p>
@@ -97,24 +97,7 @@ const Projects = () => {
                     </div>
                 
         </div>    
-        <div style={{backgroundImage:`url(${comingSoon})`}} className='transition ease-in-out delay-200 hover:-translate-y-1 hover:scale-110 duration-600  shadow-lg shadow-[rgb(126,34,206)] group container rounded-md flex justify-center items-cener mx-auto content-div'>
 
-        {/* Hover effect */}
-        <div className='py-10 opacity-0 group-hover:opacity-100'>
-            <span className='space-y-7 text-2xl font-bold text-white tracking-wider'>
-                Coming Soon...
-            </span>
-            <div className='pt-8 text-center'>
-                <a href="/"> 
-                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                </a>
-                <a href="/"> 
-                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                </a>
-
-            </div>
-        </div>
-        </div>  
             </div>
         </div>
     </div>
